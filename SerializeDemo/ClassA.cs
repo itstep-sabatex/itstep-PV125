32a761643cd17fb681ffcb5b8170ce6026c66e19
Сергій Лакас <lakas_s@itstep.academy>
2021-11-18 19:34:44 +0200

    *

---
+++ SerializeDemo/ClassA.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeDemo
+{
+    internal class Address
+    {
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+    }
+    internal class PhoneNumber
+    {
+        public string Type { get; set; }
+        public string Number { get; set; }
+    }
+
+    internal class Child
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+
+    internal class ClassA
+    {
+        public string FirstName { get; set; }
+        public string LastName  { get; set; }
+
+        public bool IsAlive { get; set; }
+        public int Age { get; set; }
+        public Address Address { get; set; }
+
+        public List<PhoneNumber> PhoneNumbers { get; set; }
+        public List<Child> Children { get; set;}
+  
+    }
+}
---
+++ SerializeDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SerializeDemo;
+
+
+var test = new ClassA
+{
+    Age = 21,
+    FirstName = "Alise",
+    LastName ="Dunli",
+    IsAlive = false,
+    Address = new Address()
+    {
+        City = "Uzhgorod",
+        State = "Transcarpatia",
+        PostalCode ="88015",
+        StreetAddress = "Lenina 60"
+    }
+
+};
+
+var json = System.Text.Json.JsonSerializer.Serialize(test);
+File.WriteAllText("test.json",json);
+
+
+var rjs = File.ReadAllText("test.json");
+var newtest = System.Text.Json.JsonSerializer.Deserialize<ClassA>(rjs);
+
+var a = 0;
+
+
