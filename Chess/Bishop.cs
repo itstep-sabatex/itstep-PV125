c379801bec2d0f5ffe45a6a252919e6a33c7810a
sabatex <serhiy.lakas@hotmail.com>
2022-02-15 19:15:27 +0200

    add bishop and rock logick

--- Chess/Bishop.cs
+++ Chess/Bishop.cs
@@ -17,7 +17,31 @@ namespace Chess
  
         public override bool CheckMove(ChessPoint source, ChessPoint destination, ChessDesk desk)
         {
-            return false;
-        }
+            if (FigureColor != desk.ActiveColor) return false;
+
+            int deltaRow = destination.Row - source.Row;
+            int deltaCol = destination.Column - source.Column;
+            if (Math.Abs(deltaRow) != Math.Abs(deltaCol)) return false;
+
+            var figure = desk[destination];
+            if (figure != null)
+            {
+                if (figure.FigureColor == this.FigureColor) return false;
+            }
+
+            int directionCol = deltaCol > 0 ? 1 : -1;
+            int directionRow = deltaRow > 0 ? 1 : -1;
+
+            // check path
+            int iteration = Math.Abs(deltaCol);
+            while (iteration > 1)
+            {
+                ChessPoint point = new ChessPoint(source.Row +directionRow *(iteration-1), source.Column + directionCol * (iteration - 1));
+                figure = desk[point];
+                if (figure != null) return false;
+                iteration--;
+            }
+            return true;
+         }
     }
 }
--- Chess/Rock.cs
+++ Chess/Rock.cs
@@ -17,6 +17,47 @@ namespace Chess
  
         public override bool CheckMove(ChessPoint source, ChessPoint destination, ChessDesk desk)
         {
+            if (FigureColor != desk.ActiveColor) return false;
+
+            int deltaRow = destination.Row - source.Row;
+            int deltaCol = destination.Column - source.Column;
+            if (deltaRow == 0 && deltaCol == 0) return false;
+            var figure = desk[destination];
+            if (figure != null)
+            {
+                if (figure.FigureColor == this.FigureColor) return false;
+            }
+            if (deltaRow == 0)
+            {
+                int directionCol = deltaCol > 0 ? 1 : -1;
+                // check path
+                int iteration = Math.Abs(deltaCol);
+                while (iteration > 1)
+                {
+                    ChessPoint point = new ChessPoint(source.Row,source.Column + directionCol*(iteration - 1));
+                    figure = desk[point];
+                    if (figure != null) return false;
+                    iteration--;
+                }
+                return true;
+            }
+
+            if (deltaCol == 0)
+            {
+                int directionRow = deltaRow > 0 ? 1 : -1;
+                // check path
+                int iteration = Math.Abs(deltaRow);
+                while (iteration > 1)
+                {
+                    ChessPoint point = new ChessPoint(source.Row +directionRow *(iteration-1), source.Column);
+                    figure = desk[point];
+                    if (figure != null) return false;
+                    iteration--;
+                }
+                return true;
+            }
+
+
             return false;
         }
     }
