7e13549a1ec100bea51a954086d44d0b78d63cf2
sabatex <serhiy.lakas@hotmail.com>
2022-03-30 19:48:18 +0300

    *

---
+++ WpfEFCoreDemo/Data/DemoDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfEFCoreDemo.Models;
+
+namespace WpfEFCoreDemo.Data
+{
+    internal class DemoDbContext:DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<StudentGroup> StudenGroups { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite(@"filename=c:\databases\PV125Demo.db");
+        }
+
+    }
+}
---
+++ WpfEFCoreDemo/Data/Migrations/20220330163955_m0.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WpfEFCoreDemo.Data.Migrations
+{
+    public partial class m0 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudenGroups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudenGroups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    StudentGroupId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Students_StudenGroups_StudentGroupId",
+                        column: x => x.StudentGroupId,
+                        principalTable: "StudenGroups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_StudentGroupId",
+                table: "Students",
+                column: "StudentGroupId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "StudenGroups");
+        }
+    }
+}
